c60ecb89bea8348464788983c19adedb8354f177
Danny Gabriel Rivera Solorzano <dan.senssei@gmail.com>
2024-04-09 03:38:24 +0200

    Redesign categories

--- app/(with-padding)/categorias/page.tsx
+++ app/(with-padding)/categorias/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Category } from "@/app/types/interfaces"
+import { Category } from "@/app/types/interfaces";
 import { IconBuildingBank } from "@tabler/icons-react";
-import prisma from "@/app/lib/db"
+import prisma from "@/app/lib/db";
 
 const getAllCategories = async () => {
     const categories = await prisma.category.findMany({
@@ -9,28 +9,30 @@ const getAllCategories = async () => {
         orderBy: { name: "asc" },
     });
     return categories;
-}
+};
 
 export default async function Categorias() {
     const categories: Category[] = await getAllCategories();
     return (
         <div className="py-5 lg:pt-0">
             <h1 className="text-2xl font-bold mb-3">Categorias</h1>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 w-full">
+            <div className="grid gap-3 w-full grid-cols-2 sm:grid-auto-13rem">
                 {categories.map((c, i) => {
                     return (
                         <Link
                             key={i}
                             href={`/categoria/${c.id}`}
-                            className="p-3 rounded-xl bg-slate-800 text-center transition-all hover:scale-105"
+                            className="rounded-xl flex items-center justify-center text-center transition-all aspect-square border-1 border-white/50 hover:bg-white/10"
                         >
-                            <div className="text-center py-4">
-                                <IconBuildingBank
-                                    className="stroke-1 mx-auto"
-                                    size={64}
-                                />
+                            <div>
+                                <div className="text-center py-4">
+                                    <IconBuildingBank
+                                        className="stroke-1 mx-auto"
+                                        size={64}
+                                    />
+                                </div>
+                                <div className="pb-3">{c.name}</div>
                             </div>
-                            <div className="pb-3">{c.name}</div>
                         </Link>
                     );
                 })}
