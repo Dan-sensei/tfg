db1cccb658f09796537151fadb5f419f49e255ba
Danny Gabriel Rivera Solorzano <dan.senssei@gmail.com>
2024-07-09 05:31:07 +0200

    Removed seed/page.tsx + force-dynamic api/categories

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,6 +1,8 @@
 import { successResponse } from "@/app/utils/util";
 import { getAllCategories } from "@/app/lib/fetchData";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
     const categories = await getAllCategories();
     return successResponse(categories);
